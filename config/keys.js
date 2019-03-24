2dcbdec1b316254bec6fe88f346b637be1ccae58
Codesworth <codezworth@gmail.com>
2019-03-24 17:07:49 +0000

    Config and Basic database connection

---
+++ config/keys.js
@@ -0,0 +1,5 @@
+
+
+module.exports = {
+    mongoURI : 'mongodb://codesworth:12shady12@ds131109.mlab.com:31109/od_hub'
+};
--- index.js
+++ index.js
@@ -1,7 +1,21 @@
 
 const express  = require('express');
+const mongoose = require('mongoose');
 
 const app = express()
+
+//DB CONFIG
+const db = require('./config/keys').mongoURI;
+
+
+//Connect to MongoDB
+
+mongoose.
+    connect(db)
+    .then(x => console.log("MongoDB Connected"))
+    .catch(err => console.log(err));
+
+    
 
 
 app.get('/', (request,response) => response.send("Hello OD!!"));
