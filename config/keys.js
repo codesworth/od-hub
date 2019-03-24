0c03a04a123ee4cf852662f42c0114eea1df4d07
Codesworth <codezworth@gmail.com>
2019-03-24 21:20:03 +0000

    Implemented Web Tokens

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,6 @@
 
 
 module.exports = {
-    mongoURI : 'mongodb://codesworth:12shady12@ds131109.mlab.com:31109/od_hub'
+    mongoURI : 'mongodb://codesworth:12shady12@ds131109.mlab.com:31109/od_hub',
+    secretOrKey: 'LordLaytonofTheHightower'
 };
--- routes/api/users.js
+++ routes/api/users.js
@@ -1,7 +1,10 @@
 
 const express = require('express');
-
+const gravatar = require('gravatar');
+const bcrypt = require('bcryptjs');
 const router = express.Router();
+const jwt = require('jsonwebtoken');
+const keys = require('../../config/keys');
 
 //Load UserModel
 const User = require('../../models/User');
@@ -20,8 +23,76 @@ router.get('/test', (req,res) => res.json({message:"User Works"}));
 //@desc    Register Users
 //@acess   Public
 
-router.get('/register',(request,response) = {
-    User
+router.post('/register',(request,response) => {
+    const {email,name,password} = request.body;
+    User.findOne({email:email}).then(user =>{
+        if (user){
+            return response.status(400).json({email:'Email Already Exists'});
+        }else{
+            const avatar = gravatar.url(email,{
+                s: '200', //size
+                r: 'pg',
+                d: 'mm'
+            });
+            const newUser = new User({
+                name:name,
+                email:email,
+                password:password,
+                avatar
+            });
+
+            bcrypt.genSalt(10, (err, salt) => {
+                bcrypt.hash(newUser.password, salt, (err,hash) => {
+                    if (err)throw err
+                    newUser.password = hash
+                    newUser.save()
+                    .then(user => {response.json(user)})
+                    .catch(err => console.log(err));
+                    
+                })
+            })
+        }
+    });
+});
+
+
+//@route   GET api/users/login
+//@desc    Login Users
+//@acess   Public
+
+router.post('/login', (request,response) => {
+    const {email, password} = request.body;
+
+    //FInd user
+
+    User.findOne({email}).then(user => {
+        if (!user){
+            return response.status(404).json({email:"User Not Found"})
+        }
+        //Checsk Password
+        bcrypt.compare(password,user.password).then(isMatch => {
+            if (isMatch){
+                //User Matched
+                const payload = {
+                    id: user.id,
+                    name: user.name,
+                    avatar: user.avatar
+                } // Create JWT Paylod
+                //Sign Token
+                jwt.sign(payload,
+                     keys.secretOrKey, 
+                     {expiresIn: 7200},
+                      (error, token) => {
+                        response.json({
+                            success: true,
+                            token: 'Bearer ' + token
+                        })
+                });
+            }else{
+                return response.status(400).json({password:'Password Incorrect'});
+            }
+        })
+    })
 });
 
 module.exports = router;
