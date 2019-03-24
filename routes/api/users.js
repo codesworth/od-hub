23be73ef2f68ea5ce654319dcd15f791e906b215
Codesworth <codezworth@gmail.com>
2019-03-24 20:32:24 +0000

    User registration

--- routes/api/users.js
+++ routes/api/users.js
@@ -3,6 +3,9 @@ const express = require('express');
 
 const router = express.Router();
 
+//Load UserModel
+const User = require('../../models/User');
+
 
 //@route   GET api/posts/test
 //@desc    Tests posts routes
@@ -11,5 +14,14 @@ const router = express.Router();
 
 
 router.get('/test', (req,res) => res.json({message:"User Works"}));
+
+
+//@route   GET api/users/register
+//@desc    Register Users
+//@acess   Public
+
+router.get('/register',(request,response) = {
+    User
+});
 
 module.exports = router;
