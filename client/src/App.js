44f0c7b11e8c36aeb8db244f05b30cfe4d41b337
Codesworth <codezworth@gmail.com>
2019-03-30 23:36:59 +0000

    Added redux and thunk. Initialized store

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,8 @@ import React, { Component } from "react";
 import "./App.css";
 
 import { BrowserRouter as Router, Route } from "react-router-dom";
-
+import { Provider } from "react-redux";
+import store from "./store";
 import Navbar from "./components/layout/Navbar";
 import Footer from "./components/layout/Footer";
 import Landing from "./components/layout/Landing";
@@ -12,17 +13,19 @@ import Register from "./components/auth/Register";
 class App extends Component {
   render() {
     return (
-      <Router>
-        <div className="App">
-          <Navbar />
-          <Route exact path="/" component={Landing} />
-          <div className="container">
-            <Route exact path="/register" component={Register} />
-            <Route exact path="/login" component={Login} />
+      <Provider store={store}>
+        <Router>
+          <div className="App">
+            <Navbar />
+            <Route exact path="/" component={Landing} />
+            <div className="container">
+              <Route exact path="/register" component={Register} />
+              <Route exact path="/login" component={Login} />
+            </div>
+            <Footer />
           </div>
-          <Footer />
-        </div>
-      </Router>
+        </Router>
+      </Provider>
     );
   }
 }
