1abbdc226e0883ff1d46f60f822eb8d4c728ff16
Codesworth <codezworth@gmail.com>
2019-03-31 19:02:36 +0000

    Registration Flow with REdux

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
-import axios from "axios";
+import { withRouter } from "react-router-dom";
 import classnames from "classnames";
-
+import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import { registerUser } from "../../actions/authActions";
 class Register extends Component {
@@ -19,6 +19,12 @@ class Register extends Component {
     this.onSubmit = this.onSubmit.bind(this);
   }
 
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.errors) {
+      this.setState({ errors: nextProps.errors });
+    }
+  }
+
   onChange = e => {
     this.setState({ [e.target.name]: e.target.value });
   };
@@ -32,20 +38,15 @@ class Register extends Component {
       password2: this.state.password2
     };
     console.log(user);
-    // axios
-    //   .post("api/users/register", user)
-    //   .then(x => console.log(x.data))
-    //   .catch(err => this.setState({ errors: err.response.data }));
-    this.props.registerUser(user);
+
+    this.props.registerUser(user, this.props.history);
   };
 
   render() {
     const { name, email, password, password2, errors } = this.state;
     const { user } = this.props.auth;
-    console.log(`Errors ${errors}`);
     return (
       <div className="register">
-        {user ? user.name : null}
         <div className="container">
           <div className="row">
             <div className="col-md-8 m-auto">
@@ -143,11 +144,18 @@ class Register extends Component {
   }
 }
 
+Register.propTypes = {
+  registerUser: PropTypes.func.isRequired,
+  auth: PropTypes.object.isRequired,
+  errors: PropTypes.object.isRequired
+};
+
 const mapStateToProps = state => ({
-  auth: state.auth
+  auth: state.auth,
+  errors: state.errors
 });
 
 export default connect(
   mapStateToProps,
   { registerUser }
-)(Register);
+)(withRouter(Register));
