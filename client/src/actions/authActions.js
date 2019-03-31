b13baa86093d5aec1b165df5ad53c5ea9fb29818
Codesworth <codezworth@gmail.com>
2019-03-31 19:17:52 +0000

    Login Action Flow

--- client/src/actions/authActions.js
+++ client/src/actions/authActions.js
@@ -1,5 +1,6 @@
 import { GET_ERRORS } from "./types";
 import axios from "axios";
+import setAuthToken from "../Utils/SetAuthToken";
 
 export const registerUser = (userData, history) => dispatch => {
   axios
@@ -9,6 +10,30 @@ export const registerUser = (userData, history) => dispatch => {
       dispatch({
         type: GET_ERRORS,
         payload: err.response.data
+      })
+    );
+};
+
+//Login Current user
+
+export const loginUser = userData => dispatch => {
+  axios
+    .post("/api/user/login", userData)
+    .then(result => {
+      //SSave to local storage
+      const { token } = result.data;
+
+      //Set Token to localStorage
+      localStorage.setItem("jwt_token", token);
+
+      //Set Token TO Auth Header
+
+      setAuthToken(token);
+    })
+    .catch(error =>
+      dispatch({
+        type: GET_ERRORS,
+        payload: error.response.data
       })
     );
 };
